fd4cd309e65e9388342ec247b5fbb4fa6dd546ce
Aviral Kapoor <kapooraviral411@gmail.com>
2024-12-01 22:50:03 +0530

    Adding CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cal';
+  StringToBeEvaluated:string = ""; //"3+1"
+
+  takeInput(num:string) {
+    //"3+" + "1" = "3+1"
+    this.StringToBeEvaluated = this.StringToBeEvaluated + num;
+  }
+
+  evaluateString() {
+    if (this.StringToBeEvaluated != "") {
+      this.StringToBeEvaluated = eval(this.StringToBeEvaluated);
+    }
+  }
+
+  clearString() {
+    this.StringToBeEvaluated = "";
+  }
+
 }
